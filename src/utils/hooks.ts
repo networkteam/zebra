6daf60d66c424ad4bd0a535b594dd75f47660d03
Philip Schmidt <philip.schmidt@networkteam.com>
2022-09-25 17:38:48 +0200

    Bugfix: prevent destructuring of 'null' context

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -4,26 +4,26 @@ import { NeosContextProps } from '../types';
 import NeosContext from './context';
 
 export const useMeta = () => {
-  const { meta } = useContext(NeosContext) as NeosContextProps;
-  return meta;
+  const context = useContext(NeosContext) as NeosContextProps;
+  return context?.meta;
 };
 
 export const useNode = () => {
-  const { node } = useContext(NeosContext) as NeosContextProps;
-  return node;
+  const context = useContext(NeosContext) as NeosContextProps;
+  return context?.node;
 };
 
 export const useDocumentNode = () => {
-  const { documentNode } = useContext(NeosContext) as NeosContextProps;
-  return documentNode;
+  const context = useContext(NeosContext) as NeosContextProps;
+  return context?.documentNode;
 };
 
 export const useSiteNode = () => {
-  const { site } = useContext(NeosContext) as NeosContextProps;
-  return site;
+  const context = useContext(NeosContext) as NeosContextProps;
+  return context?.site;
 };
 
 export const useInBackend = () => {
-  const { inBackend } = useContext(NeosContext) as NeosContextProps;
-  return inBackend;
+  const context = useContext(NeosContext) as NeosContextProps;
+  return context?.inBackend;
 };
