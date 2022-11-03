46b55e650516e6023ffbecab3680f6a61ab676ed
Philip Schmidt <philip.schmidt@networkteam.com>
2022-11-03 11:49:51 +0100

    Feature: ContentCollectionProvider

---
+++ src/lib/components/ContentCollectionProvider.tsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react';
+
+import NeosContext from '../../utils/context';
+import { useContentCollection } from '../../utils/hooks';
+import ChildNodes from './ChildNodes';
+
+type ContentCollectionProviderProps = {
+  nodeName: string;
+  children: ({ collectionProps, children }: { collectionProps: {}; children: React.ReactNode }) => React.ReactNode;
+};
+
+export default function ContentCollectionProvider({ nodeName, children }: ContentCollectionProviderProps) {
+  const neosContext = useContext(NeosContext);
+  const { collectionNode, collectionProps } = useContentCollection(nodeName);
+
+  if (!neosContext) {
+    return null;
+  }
+
+  return (
+    <NeosContext.Provider value={{ ...neosContext, node: collectionNode }}>
+      {children({ collectionProps, children: <ChildNodes /> })}
+    </NeosContext.Provider>
+  );
+}
