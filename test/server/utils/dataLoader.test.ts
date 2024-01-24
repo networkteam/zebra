b2d5dc62910cbdd30fd078a872441513ec1a9410
Christopher Hlubek <christopher.hlubek@networkteam.com>
2024-01-24 13:22:27 +0100

    Add more tests for loader functions

--- test/server/utils/dataLoader.test.ts
+++ test/server/utils/dataLoader.test.ts
@@ -1,10 +1,12 @@
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { headers } from 'next/headers';
+import { afterEach, beforeEach, describe, expect, it, test, vi } from 'vitest';
 
-import { loadDocumentProps } from '../../../src/server';
+import { loadDocumentProps, loadDocumentPropsCached, loadPreviewDocumentProps } from '../../../src/server';
+import { DataLoaderOptions } from '../../../src/types';
 
 describe('loadDocumentProps', () => {
   describe('without options', () => {
-    test('should throw an error if NEOS_BASE_URL is not set', async () => {
+    it('should throw an error if NEOS_BASE_URL is not set', async () => {
       await expect(loadDocumentProps({ slug: 'foo' })).rejects.toThrowError(
         'Missing NEOS_BASE_URL environment variable'
       );
@@ -15,7 +17,7 @@ describe('loadDocumentProps', () => {
         vi.stubEnv('NEOS_BASE_URL', 'http://neos:1234');
       });
 
-      test('should fetch from configured API', async () => {
+      it('should fetch from configured API', async () => {
         const fetch = vi.fn().mockResolvedValue(createOkayFetchResponse({ meta: { title: 'Foo' } }));
         vi.stubGlobal('fetch', fetch);
 
@@ -37,11 +39,130 @@ describe('loadDocumentProps', () => {
       await expect(loadDocumentProps({ slug: 'foo' }, { optional: true })).resolves.toBeUndefined();
     });
   });
+});
 
-  afterEach(() => {
-    vi.unstubAllEnvs();
-    vi.unstubAllGlobals();
+describe('loadDocumentPropsCached', () => {
+  beforeEach(() => {
+    vi.stubEnv('NEOS_BASE_URL', 'http://neos:1234');
   });
+
+  test('should cache the result', async () => {
+    const fetch = vi.fn().mockResolvedValue(createOkayFetchResponse({ meta: { title: 'Foo' } }));
+    vi.stubGlobal('fetch', fetch);
+
+    await expect(loadDocumentPropsCached('foo')).resolves.toStrictEqual({
+      meta: { title: 'Foo' },
+    });
+
+    await expect(loadDocumentPropsCached('foo')).resolves.toStrictEqual({
+      meta: { title: 'Foo' },
+    });
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test('should cache the result again', async () => {
+    const fetch = vi.fn().mockResolvedValue(createOkayFetchResponse({ meta: { title: 'Foo' } }));
+    vi.stubGlobal('fetch', fetch);
+
+    await expect(loadDocumentPropsCached('foo')).resolves.toStrictEqual({
+      meta: { title: 'Foo' },
+    });
+
+    await expect(loadDocumentPropsCached('foo')).resolves.toStrictEqual({
+      meta: { title: 'Foo' },
+    });
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('loadPreviewDocumentProps', () => {
+  describe('with NEOS_BASE_URL set', () => {
+    beforeEach(() => {
+      vi.stubEnv('NEOS_BASE_URL', 'http://neos:1234');
+    });
+
+    // TODO Add test without context path query param
+    // TODO Add test for PUBLIC_BASE_URL env and proxy headers
+
+    describe('with Neos session cookie', () => {
+      beforeEach(() => {
+        vi.mocked(headers).mockReturnValue(new Headers({ Cookie: 'a-session-cookie' }));
+      });
+
+      describe.each<{ opts?: DataLoaderOptions }>([
+        // No options
+        {},
+        // Override cache, should have no effect for preview
+        { opts: { cache: 'default' } },
+      ])('with options $opts', ({ opts }) => {
+        it('should fetch from configured API', async () => {
+          const fetch = vi.fn().mockResolvedValue(createOkayFetchResponse({ meta: { title: 'Foo Preview' } }));
+          vi.stubGlobal('fetch', fetch);
+
+          await expect(
+            loadPreviewDocumentProps({ 'node[__contextNodePath]': 'foo/bar@user-me' }, opts)
+          ).resolves.toStrictEqual({
+            meta: { title: 'Foo Preview' },
+          });
+
+          expect(fetch).toHaveBeenCalledWith(
+            'http://neos:1234/neos/content-api/document?contextPath=foo%2Fbar%40user-me',
+            {
+              cache: 'no-store',
+              headers: {
+                Cookie: 'a-session-cookie',
+              },
+              next: undefined,
+            }
+          );
+        });
+      });
+    });
+  });
+});
+
+let reactCached = false;
+
+// Mock the `cache` export from React:
+// 1. it is not exported in the _normal_ react version (using the canary channel would work though), but inside Next.js we can use it.
+// 2. we don't want to use the real cache, because we can not control it in tests.
+vi.mock('react', async (importOriginal) => {
+  const mod = await importOriginal<typeof import('react')>();
+  return {
+    ...mod,
+    // The cache function is a bit tricky, since it is used at the module level when importing the module,
+    // so we have to specify the implementation here and now - but still enable a reset of cached values in tests.
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    cache: vi.fn(<CachedFunction extends Function>(fn: CachedFunction): CachedFunction => {
+      // Cache the first call and return the cached value for all subsequent calls.
+      let result: any;
+      return ((...args: any[]) => {
+        if (reactCached) {
+          return result;
+        }
+        reactCached = true;
+        return (result = fn(...args));
+      }) as any;
+    }),
+  };
+});
+
+vi.mock('next/headers', async (importOriginal) => {
+  const mod = await importOriginal<typeof import('next/headers')>();
+  return {
+    ...mod,
+    headers: vi.fn(),
+  };
+});
+
+afterEach(() => {
+  vi.unstubAllEnvs();
+  vi.unstubAllGlobals();
+  vi.resetAllMocks();
+  // Make sure React cache is reset after each test
+  reactCached = false;
 });
 
 function createOkayFetchResponse(data: any) {
